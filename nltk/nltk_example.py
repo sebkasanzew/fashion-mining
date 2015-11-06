2c18a42999b789aac1a6376e1b5cff4e7dab4f5d
Raul <raulnguyen91@gmail.com>
2015-11-06 17:50:41 +0100

    ntlk: tokenization + tagging, json with nouns and sentences, list with nouns and sentence id, added fashionwords dictionary
    Former-commit-id: 57127f9a9e45045630a57a9368321ce60c0db822

--- nltk/nltk_example.py
+++ nltk/nltk_example.py
@@ -8,20 +8,41 @@ print("This is a Test for NLTK!")
 ### read data file and put every json into an array
 with open("data/plain_text.json", "r") as text_file:
     text = text_file.readlines()
-# print(text[0])
+#print(text[0])
 
-### reads first json
-json_test = json.loads(text[0])
-extracted_text = json_test["extracted_text"]
+data = []
+nouns_data = {}
+sent_id = 0
 
-#Sentence Tokenization
-sentences = nltk.sent_tokenize(extracted_text)
+for x in range(0,3):
+    ### reads first json
+    json_data = json.loads(text[x])
 
-#Word Tokenization
-for s in sentences:
-    words = nltk.word_tokenize(s)
-    print words
-    # Part of Speech Tagging
-    print(nltk.tag.pos_tag(words))
+    #print(json_data)
+    extracted_text = json_data["extracted_text"]
+
+    #Sentence Tokenization
+    sentences = nltk.sent_tokenize(extracted_text)
+    #print(sentences)
+    for s in sentences:
+        #Word tokenization and tagging
+        words_with_tags = nltk.tag.pos_tag(nltk.word_tokenize(s))
+        nouns = []
+        for w in words_with_tags:
+            if w[1] == "NN" or w[1] =="NNP" or w[1] =="NNPS" or w[1] =="NNS":
+                nouns.append(w[0])
+
+                #list with only nouns and ids
+                if (w[0] not in nouns_data):
+                    nouns_data[w[0]] = [sent_id]
+                else:
+                    # add the sent_id to the existing noun
+                    nouns_data[w[0]].append(sent_id)
+
+        data.append({"url":json_data["url"], "sent_id":sent_id, "sentence": s, "nouns": nouns})
+        sent_id += 1
+
+print json.dumps(data)
+print nouns_data
 
 
