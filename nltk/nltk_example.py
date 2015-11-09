9be1b1ba757ad9ffaa07ffe60d9ddda8d1805b61
Sebastian Kasanzew <s58457@beuth-hochschule.de>
2015-11-09 21:51:17 +0100

    code formatting applied
    Former-commit-id: a7e39603094acbf42fe3f6a320f3507697cbd36b

--- nltk/nltk_example.py
+++ nltk/nltk_example.py
@@ -5,44 +5,42 @@ import nltk
 
 print("This is a Test for NLTK!")
 
-### read data file and put every json into an array
+# read data file and put every json into an array
 with open("data/plain_text.json", "r") as text_file:
     text = text_file.readlines()
-#print(text[0])
+# print(text[0])
 
 data = []
 nouns_data = {}
 sent_id = 0
 
-for x in range(0,3):
-    ### reads first json
+for x in range(0, 3):
+    # reads first json
     json_data = json.loads(text[x])
 
-    #print(json_data)
+    # print(json_data)
     extracted_text = json_data["extracted_text"]
 
-    #Sentence Tokenization
+    # Sentence Tokenizing
     sentences = nltk.sent_tokenize(extracted_text)
-    #print(sentences)
+    # print(sentences)
     for s in sentences:
-        #Word tokenization and tagging
+        # Word tokenizing and tagging
         words_with_tags = nltk.tag.pos_tag(nltk.word_tokenize(s))
         nouns = []
         for w in words_with_tags:
-            if w[1] == "NN" or w[1] =="NNP" or w[1] =="NNPS" or w[1] =="NNS":
+            if w[1] == "NN" or w[1] == "NNP" or w[1] == "NNPS" or w[1] == "NNS":
                 nouns.append(w[0])
 
-                #list with only nouns and ids
-                if (w[0] not in nouns_data):
+                # list with only nouns and ids
+                if w[0] not in nouns_data:
                     nouns_data[w[0]] = [sent_id]
                 else:
                     # add the sent_id to the existing noun
                     nouns_data[w[0]].append(sent_id)
 
-        data.append({"url":json_data["url"], "sent_id":sent_id, "sentence": s, "nouns": nouns})
+        data.append({"url": json_data["url"], "sent_id": sent_id, "sentence": s, "nouns": nouns})
         sent_id += 1
 
 print json.dumps(data)
 print nouns_data
-
-
