8eb1998668a57b67abbc1944d8695a5294c6c04c
Sebastian Kasanzew <s58457@beuth-hochschule.de>
2015-11-09 22:00:15 +0100

    changed indent back to 4 spaces fixed all warnings

--- gensim-tutorial/gensim_vector_example.py
+++ gensim-tutorial/gensim_vector_example.py
@@ -3,13 +3,15 @@
 
 import logging
 
+from gensim import corpora  # , models, similarities
+from collections import defaultdict
+from pprint import pprint
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-###Here insert connection to NLTK - JSON INPUT
+# Here insert connection to NLTK - JSON INPUT
 
 print("#################### From Strings to Vectors ########################")
-
-from gensim import corpora, models, similarities
 
 documents = ["Kanye West brings new Nike sneaker collection",
              "One shoe to conquer the world in New York",
@@ -21,21 +23,17 @@ documents = ["Kanye West brings new Nike sneaker collection",
              "This jeans from Katy Perry was old-fashioned",
              "Every people want the new Nike Sneaker from Kanye"]
 
-stoplist = set('for a of the and to in is are'.split())
-texts = [[word for word in document.lower().split() if word not in stoplist]
+stop_list = set('for a of the and to in is are'.split())
+texts = [[word for word in document.lower().split() if word not in stop_list]
          for document in documents]
-
-from collections import defaultdict
 
 frequency = defaultdict(int)
 for text in texts:
-  for token in text:
-    frequency[token] += 1
+    for token in text:
+        frequency[token] += 1
 
 texts = [[token for token in text if frequency[token] > 1]
          for text in texts]
-
-from pprint import pprint
 
 pprint(texts)
 
@@ -61,24 +59,24 @@ print("################### Sequential Document Vector load ####################"
 
 
 class MyCorpus(object):
-  def __iter__(self):
-    for line in open('mycorpus.txt'):
-      # assume there's one document per line, tokens separated by whitespace
-      yield dictionary.doc2bow(line.lower().split())
+    def __iter__(self):
+        for line in open('mycorpus.txt'):
+            # assume there's one document per line, tokens separated by whitespace
+            yield dictionary.doc2bow(line.lower().split())
 
 
 corpus_memory_friendly = MyCorpus()  # doesn't load the corpus into memory!
 print(corpus_memory_friendly)
 
 for vector in corpus_memory_friendly:  # load one vector into memory at a time
-  print(vector)
+    print(vector)
 
 print("\n")
 print("################### Construct Dict sequential ####################")
 # collect statistics about all tokens
 dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
 # remove stop words and words that appear only once
-stop_ids = [dictionary.token2id[stopword] for stopword in stoplist if stopword in dictionary.token2id]
+stop_ids = [dictionary.token2id[stopword] for stopword in stop_list if stopword in dictionary.token2id]
 once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
 dictionary.filter_tokens(stop_ids + once_ids)  # remove stop words and words that appear only once
 dictionary.compactify()  # remove gaps in id sequence after words that were removed
