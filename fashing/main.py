e0bad2e3a8e2711819108f8c7049c39b9862f653
Sebastian Kasanzew <s58457@beuth-hochschule.de>
2015-11-12 21:47:16 +0100

    first iteration of project restructure
    Former-commit-id: c5a0d600689be448f26d3db5e747c861319f628c

---
+++ fashing/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import nltk
+import lib.word2vec.word2vec as w2v
+
+
+def main():
+    print "test"
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# TODO complete the setup file
+setup(
+    name='fashing',
+    version='0.1',
+    description='fashion mining application',
+    author='Benjamin Fischer, Raul Vinh Khoa Nguyen, Sebastian Kasanzew, Sebastian Krawczyk',
+    author_email='...@..., ...@..., sebkasanzew@gmail.com, ...@...',
+    packages=["nltk"]
+)
