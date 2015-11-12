0f572ed2cef6fa85b155572bdff5562fdd111e45
Benjamin Fischer <benjamin.fischer@ageto.net>
2015-11-12 19:27:29 +0100

    workflow pipeline from nltk to similarity interface with dictionary word "tuxedo"
    Former-commit-id: fa0cdfc645fe14f5029cedcfd25354ffaf0d05bd

--- gensim-tutorial/word2vec.py
+++ gensim-tutorial/word2vec.py
@@ -15,37 +15,77 @@ from pprint import pprint
 # Create Dictionary
 path = '/tmp/zalando.dict'
 
-if os.path.exists(path) == False:
-    print('create dictionary')
-    data = [[word.strip('[],')[1:-1] for word in tempList.split()] for tempList in open("../dict.txt", "r")]
-    dictionary = corpora.Dictionary(data)
-    dictionary.save(path)
-    print(dictionary)
-else:
-  dictionary = corpora.Dictionary.load(path)
+# if os.path.exists(path) == False:
+#     print('create dictionary')
+#     data = [[word.strip('[],')[1:-1] for word in tempList.split()] for tempList in open("../dict.txt", "r")]
+#     dictionary = corpora.Dictionary(data)
+#     dictionary.save(path)
+#     print(dictionary)
+# else:
+#   dictionary = corpora.Dictionary.load(path)
 
 
-print(dictionary.token2id)
+#print(dictionary.token2id)
 #Example document
 documents = [ "sandal with jacket dress vest tuxedo",
-             "brief gown" ]
+             "brief gown here is what you",
+              "sandal without jacket",
+              "to brief a person",
+              "tuxedo"]
 
 tokens = []
 token = 0
-corpus = []
+data = []
+nouns = []
 for document in documents:
   sentences = nltk.sent_tokenize(document)
 
 # nltk version 3.1 beacuse in 3.0 pos_tag doesnt work
-
+# tokenize sentences and add nouns to array
   for s in sentences:
-    tokens = nltk.word_tokenize(s)
+    s.lower()
+    tokens.append(nltk.word_tokenize(s))
+    words_with_tags = nltk.tag.pos_tag(nltk.word_tokenize(s))
 
-  corpus.append(dictionary.doc2bow(tokens))
 
+    for w in words_with_tags:
+            if w[1] == "NN" or w[1] == "NNP" or w[1] == "NNPS" or w[1] == "NNS":
+              nouns.append(w[0])
 
+  data.append(nouns)
+
+######create dictionary with unique tokens
+######array of tokens with nouns from nltk
+dictionary = corpora.Dictionary(data)
+print(dictionary.token2id)
+dictionary.save(path)
+
+######match dict with documents
+corpus = [dictionary.doc2bow(text) for text in tokens]
 corpora.MmCorpus.serialize('/tmp/zalando.mm', corpus)
 #corpus = corpora.MmCorpus('/tmp/zalando.mm')
-print(corpus)
+
+# print(nouns)
+# print(tokens)
+# print(dictionary)
+# print (corpus)
+
+
+#####Similarity Interface############
+####doc should be dictionary with fashion words
+doc = "tuxedo"
+lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]
+print(vec_lsi)
+
+
+######Similarity Query######
+index = similarities.MatrixSimilarity(lsi[corpus])
+index.save('/tmp/zalando.index')
+index = similarities.MatrixSimilarity.load('/tmp/zalando.index')
+sims = index[vec_lsi]
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print(sims)
 
 
