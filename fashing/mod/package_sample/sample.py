967fca156f117105405b92ae8f1c06603b385a4e
Sebastian Kasanzew <s58457@beuth-hochschule.de>
2015-11-16 11:00:55 +0100

    fourth iteration of project restructure
    Former-commit-id: 48cb60c64dcc57836d1de111096f7c667ae6be0d

---
+++ fashing/mod/package_sample/sample.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import importlib
+import mod.nltk_example
+
+
+def main():
+    """This is the main function"""
+    print("main executed")
+
+
+def package_sample():
+    """This is an example of a function"""
+    print("this is a sample")
+
