078947fb93906be03c16b311d6ef927de31896ce
Benjamin Fischer <benjamin.fischer@ageto.net>
2015-11-16 13:59:50 +0100

    use fashion dict
    Former-commit-id: 21d609ef418588ce6114cd471c79d8451f7c6e86

--- fashing/mod/word2vec.py
+++ fashing/mod/word2vec.py
@@ -3,54 +3,63 @@
 
 import logging
 import nltk
+import json
 import os
 import sys
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# from nltk import pos_tag, word_tokenize, sent_tokenize
 from gensim import corpora, models, similarities
 from pprint import pprint
 
 # Create Dictionary
 path = '/tmp/'
 
-# if os.path.exists(path) == False:
-#     print('create dictionary')
-#     data = [[word.strip('[],')[1:-1] for word in tempList.split()] for tempList in open("../dict.txt", "r")]
-#     dictionary = corpora.Dictionary(data)
-#     dictionary.save(path)
-#     print(dictionary)
-# else:
-#   dictionary = corpora.Dictionary.load(path)
+# read data file and put every json into an array
+with open("../../../data/plain_text.json", "r") as text_file:
+  text = text_file.readlines()
 
+with open("../../../data/fashion-words.txt", "r") as text_file:
+  f_words = text_file.readlines()
 
-#print(dictionary.token2id)
-#Example document
-documents = [ "sandal with jacket dress vest tuxedo",
+# Example document
+documents = ["sandal with jacket dress vest tuxedo",
              "brief gown here is what you",
-              "sandal without jacket",
-              "to brief a person",
-              "tuxedo"]
+             "sandal without jacket",
+             "to brief a person",
+             "tuxedo"]
 
 tokens = []
 token = 0
 data = []
 nouns = []
+test = []
+
+print(f_words)
+for word in f_words:
+  for dic in word.lower().split():
+    print(dic)
+    test.append([dic.strip('[],')[1:-1]])
+
 for document in documents:
+  # for x in range(0, 1):
+  # reads first json
+  # json_data = json.loads(text[x])
+
+  # print(json_data)
+  # extracted_text = json_data["extracted_text"]
   sentences = nltk.sent_tokenize(document)
 
-# nltk version 3.1 beacuse in 3.0 pos_tag doesnt work
-# tokenize sentences and add nouns to array
+  # nltk version 3.1 beacuse in 3.0 pos_tag doesnt work
+  # tokenize sentences and add nouns to array
   for s in sentences:
     s.lower()
     tokens.append(nltk.word_tokenize(s))
     words_with_tags = nltk.tag.pos_tag(nltk.word_tokenize(s))
 
-
     for w in words_with_tags:
-            if w[1] == "NN" or w[1] == "NNP" or w[1] == "NNPS" or w[1] == "NNS":
-              nouns.append(w[0])
+      if w[1] == "NN" or w[1] == "NNP" or w[1] == "NNPS" or w[1] == "NNS":
+        nouns.append(w[0])
 
   data.append(nouns)
 
@@ -63,29 +72,26 @@ dictionary.save(path + 'zalando.dict')
 ######match dict with documents
 corpus = [dictionary.doc2bow(text) for text in tokens]
 corpora.MmCorpus.serialize(path + 'zalando.mm', corpus)
-#corpus = corpora.MmCorpus('/tmp/zalando.mm')
+# corpus = corpora.MmCorpus('/tmp/zalando.mm')
 
 # print(nouns)
 # print(tokens)
 # print(dictionary)
-# print (corpus)
-
+print (corpus)
 
 #####Similarity Interface############
 ####doc should be dictionary with fashion words
-doc = "tuxedo"
+docs = test
 lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_bow = [dictionary.doc2bow(doc) for doc in docs]
+
 vec_lsi = lsi[vec_bow]
 print(vec_lsi)
-
 
 ######Similarity Query######
 index = similarities.MatrixSimilarity(lsi[corpus])
 index.save(path + 'zalando.index')
 index = similarities.MatrixSimilarity.load(path + 'zalando.index')
 sims = index[vec_lsi]
-sims = sorted(enumerate(sims), key=lambda item: -item[1])
+# sims = sorted(enumerate(sims), key=lambda item: -item[1])
 print(sims)
-
-
