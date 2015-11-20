90e66defbd0272fe7bf57bdcf59d561fa3fe1c01
Benjamin Fischer <benjamin.fischer@ageto.net>
2015-11-20 20:47:31 +0100

    added word2vec trainer for json file, with tokenizer
    Former-commit-id: d063f13af5d20df9c23d1b77d7eedcc374c1b5a0

---
+++ fashing/mod/train_w2v.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import pickle
+import nltk
+import json
+import os
+import sys
+from gensim.models import Word2Vec
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# read data file and put every json into an array
+with open("../../data/plain_text.json", "r") as text_file:
+  text = text_file.readlines()
+
+with open("../../data/fashion-words.txt", "r") as text_file:
+  f_words = text_file.readlines()
+
+token_file = open("../../data/token_words.txt", "w")
+
+length = len(text)
+
+for doc in text:
+  # reads first json
+  json_data = json.loads(doc)
+
+  # print(json_data)
+  extracted_text = json_data["extracted_text"]
+  sentences = nltk.sent_tokenize(extracted_text)
+
+  # nltk version 3.1 beacuse in 3.0 pos_tag doesnt work
+  # tokenize sentences and add nouns to array
+  tokens = []
+  for s in sentences:
+    s.lower()
+    s = nltk.word_tokenize(s)
+    tokens.append(s)
+    #pickle.dump(s, token_file)
+
+  pickle.dump(tokens, token_file)
+  if ( text.index(doc) % 1000 == 0 ):
+    print str(text.index(doc)) + " from " + str(length) + " senetences tokenized"
+
+class MySentence(object):
+
+  def __iter__(self):
+  #Pickle streams are entirely self-contained,
+  # and so unpickling will unpickle one object at a time
+    while 1:
+     try:
+        p = pickle.load(token_file)
+        for l in p:
+          yield l
+
+     except EOFError:
+         break
+
+token_file = open("../../data/token_words.txt", "r")
+sentences = MySentence()
+model = Word2Vec([doc for doc in sentences])
+model.save('/../../data/fashion_model')
+
