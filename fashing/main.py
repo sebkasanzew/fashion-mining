2fcecc627c15794b4ef3c0e9f5afb7581aa8228c
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-12-07 21:19:53 +0100

    updated main GUI
    Former-commit-id: 798a9d3061e1df78d68b00f930ca92abdca8155d

--- fashing/main.py
+++ fashing/main.py
@@ -3,56 +3,74 @@
 
 
 from Tkinter import *
-import fashing.mod.graph as graph
+import ttk
+# import subprocess as sub
+# import fashing.mod.graph as graph
 
 
 def main():
+    # styling properties with ttk
+    # style = ttk.Style()
+    # style.configure("TButton", padding=6, relief="flat")
+
     root = Tk()
     app = Application(master=root)
     app.mainloop()
-    root.destroy()
 
 
 class Application(Frame):
-    def say_hi(self):
-        print "hi there, everyone!"
 
     def __init__(self, master=None):
+        # console output for the GUI
+        # p = sub.Popen('mod/word2vec.py', stdout=sub.PIPE, stderr=sub.PIPE)
+        # self.output, self.errors = p.communicate()
+
         Frame.__init__(self, master)
         self.pack()
         self.create_widgets()
         self.create_canvas()
+        self.create_console_log()
 
     def create_widgets(self):
-        print "null"
-        # self.QUIT = Button(self)
-        # self.QUIT["text"] = "QUIT"
-        # self.QUIT["fg"] = "red"
-        # self.QUIT["command"] = self.quit
+        self.DRAW = ttk.Button(self)
+        self.DRAW["text"] = "Draw"
+        self.DRAW["command"] = self.button_draw()
+        self.DRAW.pack({"side": "bottom"})
 
-        # self.QUIT.pack({"side": "left"})
-
-        # self.hi_there = Button(self)
-        # self.hi_there["text"] = "Hello",
-        # self.hi_there["command"] = self.say_hi
-
-        # self.hi_there.pack({"side": "left"})
+    def button_draw(self):
+        return "TODO"
 
     def create_canvas(self):
-        self.CANVAS = Canvas(self, width=500, height=500, background='white')
+        self.CANVAS_WIDTH = 500
+        self.CANVAS_HEIGHT = 300
+
+        self.CANVAS = Canvas(self, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT, background='white')
         self.CANVAS.pack()
-
-        # self.CANVAS.create_line(10, 10, 10, 490, fill="#333", width=2)
-        # self.CANVAS.create_line(10, 490, 490, 490, fill="#333", width=2)
-
         self.draw_canvas()
 
+    def create_console_log(self):
+        self.CONSOLE_CONTAINER = ttk.LabelFrame(self, text="This is the container")
+        self.CONSOLE_CONTAINER.pack(fill="both", expand="yes")
+
+        self.CONSOLE = ttk.Label(self.CONSOLE_CONTAINER, text="this is the console")
+        self.CONSOLE.pack()
+
+        self.CONSOLE_TEXT = Text(self)
+        self.CONSOLE_TEXT.pack()
+        # self.CONSOLE_TEXT.insert(END, self.output)
 
     def draw_canvas(self):
         canvas = self.CANVAS
+        padding = 10
+        min_y = 0 + padding
+        min_x = 0 + padding
+        max_y = self.CANVAS_HEIGHT - padding
+        max_x = self.CANVAS_WIDTH - padding
 
-        canvas.create_line(10, 10, 10, 490, fill="#333", width=2)
-        canvas.create_line(10, 490, 490, 490, fill="#333", width=2)
+        # create the graph axes
+        canvas.create_line(min_x, max_y, min_x, min_y, fill="#333", width=2)  # y axis
+        canvas.create_line(min_x, max_y, max_x, max_y, fill="#333", width=2)  # x axis
+        # canvas.create_polygon(10, 20, 30, fill="#333")
 
 
 if __name__ == "__main__":
