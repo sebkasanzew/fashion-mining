6de28cb46e71d0a582cfe1a6bc6b96d355033b32
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-12-13 16:35:39 +0100

    implement graph drawing with test values in GUI
    Former-commit-id: 73b197ed1e491d14c0c5e33bacad427702c03778

--- fashing/main.py
+++ fashing/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-from Tkinter import *
+import Tkinter as tk
 import ttk
 # import subprocess as sub
 # import fashing.mod.graph as graph
@@ -14,25 +13,43 @@ def main():
     # style = ttk.Style()
     # style.configure("TButton", padding=6, relief="flat")
 
-    root = Tk()
-    app = Application(master=root)
-    app.mainloop()
+    # print(w2v.word2vec())
 
-    print(w2v.word2vec())
+    root = tk.Tk()
+    Application(master=root).pack(side="top", fil="both", expand=True)
+    root.mainloop()
 
 
-class Application(Frame):
-
-    def __init__(self, master=None):
+class Statusbar(tk.Frame):
+    def __init__(self, master=None, *args, **kwargs):
         # console output for the GUI
         # p = sub.Popen('mod/word2vec.py', stdout=sub.PIPE, stderr=sub.PIPE)
         # self.output, self.errors = p.communicate()
 
-        Frame.__init__(self, master)
-        self.pack()
-        self.create_widgets()
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        # self.create_console_log()
+
+    def create_console_log(self):
+        self.CONSOLE_CONTAINER = ttk.LabelFrame(self, text="This is the container")
+        self.CONSOLE_CONTAINER.pack(fill="both", expand="yes")
+
+        self.CONSOLE = ttk.Label(self.CONSOLE_CONTAINER, text="this is the console")
+        self.CONSOLE.pack()
+
+        self.CONSOLE_TEXT = tk.Text(self)
+        self.CONSOLE_TEXT.pack()
+        # self.CONSOLE_TEXT.insert(END, self.output)
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.statusbar = Statusbar(self)
+
+        self.statusbar.pack(side="bottom", fill="x")
+
+        # self.create_widgets()
         self.create_canvas()
-        self.create_console_log()
 
     def create_widgets(self):
         self.DRAW = ttk.Button(self)
@@ -47,20 +64,9 @@ class Application(Frame):
         self.CANVAS_WIDTH = 500
         self.CANVAS_HEIGHT = 300
 
-        self.CANVAS = Canvas(self, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT, background='white')
+        self.CANVAS = tk.Canvas(self, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT, background='white')
         self.CANVAS.pack()
         self.draw_canvas()
-
-    def create_console_log(self):
-        self.CONSOLE_CONTAINER = ttk.LabelFrame(self, text="This is the container")
-        self.CONSOLE_CONTAINER.pack(fill="both", expand="yes")
-
-        self.CONSOLE = ttk.Label(self.CONSOLE_CONTAINER, text="this is the console")
-        self.CONSOLE.pack()
-
-        self.CONSOLE_TEXT = Text(self)
-        self.CONSOLE_TEXT.pack()
-        # self.CONSOLE_TEXT.insert(END, self.output)
 
     def draw_canvas(self):
         canvas = self.CANVAS
@@ -69,11 +75,45 @@ class Application(Frame):
         min_x = 0 + padding
         max_y = self.CANVAS_HEIGHT - padding
         max_x = self.CANVAS_WIDTH - padding
+        x_range = max_x - min_x
+        y_range = max_y - min_y
+
+        grid_resolution = 50
+
+        # create a grid
+        for i in xrange(min_x, max_x, grid_resolution):
+            canvas.create_line(i, min_y, i, max_y)
+
+        for i in xrange(max_y, min_y, -grid_resolution):
+            canvas.create_line(min_x, i, max_x, i)
+
+        # create closing grid lines
+        canvas.create_line(max_x, min_y, max_x, max_y)
+        canvas.create_line(min_x, min_y, max_x, min_y)
 
         # create the graph axes
         canvas.create_line(min_x, max_y, min_x, min_y, fill="#333", width=2)  # y axis
         canvas.create_line(min_x, max_y, max_x, max_y, fill="#333", width=2)  # x axis
-        # canvas.create_polygon(10, 20, 30, fill="#333")
+        canvas.create_polygon((10, 20, 30, 40, 20, 30), fill="#333")  # triangle not working
+
+        self.graph_data = [
+            [0, 0],
+            [.2, .1],
+            [.4, .5],
+            [.6, .9],
+            [1, 1]
+        ]
+
+        pre = []  # temp value for the previous iteration
+        for val in self.graph_data:
+            if pre:
+                x_start = pre[0] * x_range + min_x
+                y_start = pre[1] * y_range + min_y
+                x_end = val[0] * x_range + min_x
+                y_end = val[1] * y_range + min_y
+                canvas.create_line(x_start, y_start, x_end, y_end, fill="#F00")
+
+            pre = val
 
 
 if __name__ == "__main__":
