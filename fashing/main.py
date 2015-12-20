ddac8de9bfc1f3a1ebf242b36c204dcbd2359adf
Sebastian Kasanzew <sebkasanzew@gmail.com>
2015-12-20 14:58:52 +0100

    main.py refactoring and optimization
    Former-commit-id: 86899caa180f5a955fe0a71777a21592267708c8

--- fashing/main.py
+++ fashing/main.py
@@ -6,16 +6,19 @@ import Tkinter as tk
 import ttk
 import sys
 # import subprocess as sub
-# import fashing.mod.graph as graph
 import mod.util as util
 import mod.word2vec as w2v
 
 COLOR_WHITE = '#FFF'
+COLOR_BLACK = '#000'
 COLOR_GRAY_LIGHT = "#CCC"
 COLOR_GRAY_DARK = "#333"
 COLOR_LINE_ONE = "#F00"
 
 WIDTH_LINE_ONE = 5
+CANVAS_BACKGROUND_COLOR = COLOR_WHITE
+CANVAS_AXIS_COLOR = COLOR_GRAY_DARK
+CANVAS_GRID_COLOR = COLOR_GRAY_LIGHT
 
 FONT_MENU = ("Myriad Pro", 14)
 FONT_GRAPH = ("Myriad Pro", 24)
@@ -57,6 +60,11 @@ class Statusbar(tk.Frame):
 
 
 class Application(tk.Frame):
+    canvas_padding = 100
+    canvas_grid_sections = 5
+    canvas_x_headline = "Recall"
+    canvas_y_headline = "Precision"
+
     def __init__(self, master=None, *args, **kwargs):
         self.CANVAS_WIDTH = 1000
         self.CANVAS_HEIGHT = 1000
@@ -110,8 +118,8 @@ class Application(tk.Frame):
         self.menubar = tk.Menu(self, font=FONT_MENU)
 
         self.file_menu = tk.Menu(self.menubar, tearoff=0, font=FONT_MENU)
-        self.file_menu.add_command(label="Open", command=self.open_file)
-        self.file_menu.add_command(label="Save", command=self.open_save)
+        self.file_menu.add_command(label="Open", command=lambda: self.open_file())
+        self.file_menu.add_command(label="Save", command=lambda: self.open_save())
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Exit", command=self.quit)
 
@@ -136,15 +144,34 @@ class Application(tk.Frame):
         self.DRAW.pack({"side": "bottom"})
 
     def create_canvas(self):
-        self.CANVAS = tk.Canvas(self, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT, background=COLOR_WHITE)
+        self.CANVAS = tk.Canvas(self, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT,
+                                background=CANVAS_BACKGROUND_COLOR)
         self.CANVAS.pack()
         self.draw_canvas()
 
-    def update_canvas(self, grid_sections):
-        self.CANVAS.delete("all")
-        self.draw_canvas(grid_sections=grid_sections)
+    def update_canvas(self, padding=canvas_padding, x_headline=canvas_x_headline, y_headline=canvas_y_headline,
+                      grid_sections=canvas_grid_sections):
+        canvas_padding = padding
+        canvas_x_headline = x_headline
+        canvas_y_headline = y_headline
+        canvas_grid_sections = grid_sections
 
-    def draw_canvas(self, padding=100, x_headline="Precision", y_headline="Recall", grid_sections=5):
+        self.CANVAS.delete("all")
+        self.CANVAS.config(width=self.CANVAS_WIDTH,
+                           height=self.CANVAS_HEIGHT,
+                           background=CANVAS_BACKGROUND_COLOR)
+
+        self.draw_canvas(padding=canvas_padding,
+                         x_headline=canvas_x_headline,
+                         y_headline=canvas_y_headline,
+                         grid_sections=canvas_grid_sections)
+
+    def update_canvas_dimensions(self):
+        self.CANVAS.config(width="", height="")
+        self.draw_canvas()
+
+    def draw_canvas(self, padding=canvas_padding, x_headline=canvas_x_headline, y_headline=canvas_y_headline,
+                    grid_sections=canvas_grid_sections):
         canvas = self.CANVAS
         min_y = 0 + padding
         min_x = 0 + padding
@@ -165,7 +192,7 @@ class Application(tk.Frame):
             else:
                 axis_label = util.format_number(axis_label)
 
-            canvas.create_line(i, min_y, i, max_y, dash=(5, 5), fill=COLOR_GRAY_LIGHT)
+            canvas.create_line(i, min_y, i, max_y, dash=(5, 5), fill=CANVAS_GRID_COLOR)
             canvas.create_text(min_x - FONT_GRAPH[1], i, text=axis_label, font=FONT_GRAPH)
 
         for i in xrange(max_y, min_y, -(y_range // grid_sections)):
@@ -177,17 +204,17 @@ class Application(tk.Frame):
             else:
                 axis_label = util.format_number(axis_label)
 
-            canvas.create_line(min_x, i, max_x, i, dash=(5, 5), fill=COLOR_GRAY_LIGHT)
+            canvas.create_line(min_x, i, max_x, i, dash=(5, 5), fill=CANVAS_GRID_COLOR)
             canvas.create_text(i, max_y + FONT_GRAPH[1], text=axis_label, font=FONT_GRAPH)
 
         # create closing grid lines
-        canvas.create_line(max_x, min_y, max_x, max_y, dash=(5, 5), fill=COLOR_GRAY_LIGHT)
-        canvas.create_line(min_x, min_y, max_x, min_y, dash=(5, 5), fill=COLOR_GRAY_LIGHT)
+        canvas.create_line(max_x, min_y, max_x, max_y, dash=(5, 5), fill=CANVAS_GRID_COLOR)
+        canvas.create_line(min_x, min_y, max_x, min_y, dash=(5, 5), fill=CANVAS_GRID_COLOR)
 
         # create the graph axes
-        canvas.create_line(min_x, max_y, min_x, min_y, fill=COLOR_GRAY_DARK, width=2)  # y axis
-        canvas.create_line(min_x, max_y, max_x, max_y, fill=COLOR_GRAY_DARK, width=2)  # x axis
-        canvas.create_polygon((10, 20, 30, 40, 20, 30), fill=COLOR_GRAY_DARK)  # triangle not working
+        canvas.create_line(min_x, max_y, min_x, min_y, fill=CANVAS_AXIS_COLOR, width=2)  # y axis
+        canvas.create_line(min_x, max_y, max_x, max_y, fill=CANVAS_AXIS_COLOR, width=2)  # x axis
+        canvas.create_polygon((10, 20, 30, 40, 20, 30), fill=CANVAS_AXIS_COLOR)  # triangle not working
 
         # create the axis headlines
         x_headline_horizontal_pos = (max_x + padding) / 2
@@ -196,7 +223,8 @@ class Application(tk.Frame):
         y_headline_vertical_pos = (max_y + padding) / 2
 
         canvas.create_text(x_headline_horizontal_pos, x_headline_vertical_pos, text=x_headline, font=FONT_GRAPH)
-        canvas.create_text(y_headline_horizontal_pos, y_headline_vertical_pos, text=y_headline, font=FONT_GRAPH, angle=90)
+        canvas.create_text(y_headline_horizontal_pos, y_headline_vertical_pos, text=y_headline, font=FONT_GRAPH,
+                           angle=90)
 
         # create the axis texts
         canvas.create_text(min_x - FONT_GRAPH[1], max_y + FONT_GRAPH[1], text="0", font=FONT_GRAPH)
