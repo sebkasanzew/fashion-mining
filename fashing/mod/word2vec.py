33ad6169163d7971c74cc91a018413173637e280
Raul <raulnguyen91@gmail.com>
2016-01-04 19:38:49 +0100

    refactor #1
    Former-commit-id: 9b1ba59cb9c5dd75dcfff17853b2b6b8c285d0e3

--- fashing/mod/word2vec.py
+++ fashing/mod/word2vec.py
@@ -17,323 +17,261 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 # Global Paths TODO: improvement of dirs
 PROJECT_DIR = os.path.dirname(__file__) + "/../../"
-# GLOVE_DIR = '/opt/word2vec/common_words'
 GLOVE_DIR = PROJECT_DIR + "data/tmp/"
 
-
-def main():
-  """The main test executable"""
-  # Here goes the code from below
-  # Create Dictionary
-
-  ######create dictionary with unique tokens
-  ######array of tokens with nouns from nltk
-  #dictionary = corpora.Dictionary(data)
-  #print(dictionary.token2id)
-  #dictionary.save(path + 'zalando.dict')
-
-  ######match dict with documents
-  #corpus = [dictionary.doc2bow(text) for text in tokens]
-  #corpora.MmCorpus.serialize(path + 'zalando.mm', corpus)
-  # corpus = corpora.MmCorpus('/tmp/zalando.mm')
-
-  # print(nouns)
-  # print(tokens)
-  # print(dictionary)
-
-  # #####Similarity Interface############
-  # ####doc should be dictionary with fashion words
-  # docs = test
-  # lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-  # vec_bow = [dictionary.doc2bow(doc) for doc in docs]
-  #
-  # vec_lsi = lsi[vec_bow]
-  # print(vec_lsi)
-  #
-  # ######Similarity Query######
-  # index = similarities.MatrixSimilarity(lsi[corpus])
-  # index.save(path + 'zalando.index')
-  # index = similarities.MatrixSimilarity.load(path + 'zalando.index')
-  # sims = index[vec_lsi]
-  # # sims = sorted(enumerate(sims), key=lambda item: -item[1])
-  # print(sims)
-
-
+#TODO: refactor code which draws the table
 def word2vec():
-  # Loading external files
-  with open(PROJECT_DIR + "data/example_docs_tokenized.json", "r") as text_file:
-    text = json.load(text_file)
-  with open(PROJECT_DIR + "data/one_word_entities_reduced.txt", "r") as text_file:
-    fashion_dictionary = text_file.readlines()
-  with open(PROJECT_DIR + "data/vector_words_tags.json", "a") as vector_words:
-    vector_words.seek(0)
-    vector_words.truncate()
-  # with open(PROJECT_DIR + "data/fashion-words.txt", "r") as text_file:
-  #  fashion_dictionary = text_file.readlines()
+    """
+    executes the main logic of word2vec
+    :return: result-json with tagged words, indices and cosine distance
+    """
+    # Loading external files
+    with open(PROJECT_DIR + "data/example_docs_tokenized.json", "r") as text_file:
+        text = json.load(text_file)
+    with open(PROJECT_DIR + "data/one_word_entities_reduced.txt", "r") as text_file:
+        fashion_dictionary = text_file.readlines()
+    with open(PROJECT_DIR + "data/vector_words_tags.json", "a") as vector_words:
+        vector_words.seek(0)
+        vector_words.truncate()
+    #with open(PROJECT_DIR + "data/fashion-words.txt", "r") as text_file:
+    #    fashion_dictionary = text_file.readlines()
 
 
-  # read words from fashion dictionary
-  fashion_words = []
-  for word in fashion_dictionary:
-    for dic in word.lower().split():
-      fashion_words.append([dic.strip('[],')[1:-1]])
+    # read words from fashion dictionary
+    fashion_words = []
+    for word in fashion_dictionary:
+        for dic in word.lower().split():
+            fashion_words.append([dic.strip('[],')[1:-1]])
 
-  # nltk tokenizing with demo sents
-  demo_sents = [
-    "British designer Nadia Izruna’s love of clothing and the desire to sew up cheerful, well-designed womenswear spurred her on to start her own label in 2009.",
-    "During Paris Fashion Week I had the opportunity to work on something incredibly special for Valentino and vogue.com."]
+    # load model for word2vec
+    model = load_model(1)
 
-  # load model for word2vec
-  model = load_model(1)
+    number_of_documents = 0
+    max_documents = 50
+    sim = 0
 
+    for doc in text:
+        cos_dist = []
+        extracted_tokens = doc["entities"]
+        for token in extracted_tokens:
+            for f in fashion_words:
+                try:
+                    # cos = model.similarity("/en/" + w[0], "/en/" + f[0])
+                    cos = model.similarity(token.lower(), f[0].lower())
+                    if cos > sim:
+                        sim = cos
+                        word = f[0]
+                except:
+                    pass
 
-  number_of_documents = 0
-  max_documents = 50
-  sim = 0
-  list_of_words = []
-  for doc in text:
-    cos_dist = []
-    extracted_tokens = doc["entities"]
-    for token in extracted_tokens:
-      for f in fashion_words:
-        try:
-          # cos = model.similarity("/en/" + w[0], "/en/" + f[0])
-          cos = model.similarity(token.lower(), f[0].lower())
-          if cos > sim:
-            sim = cos
-            word = f[0]
-        except:
-          pass
+            # appending JOIN-Partner
+            if sim == 0:
+                cos_dist.append(["NONE", "NONE"])
+            else:
+                cos_dist.append([str(word), str(sim)])
+                sim = 0
 
-    # appending JOIN-Partner
-      if sim == 0:
-        cos_dist.append(["NONE", "NONE"])
-      else:
-        cos_dist.append([str(word), str(sim)])
+        doc["cosDist"] = cos_dist
 
-        sim = 0
+        pre = ""
+        if number_of_documents == 0:
+            pre = "["
+            suf = ","
+        elif number_of_documents == max_documents - 1:
+            suf = "]"
+        else:
+            suf = ","
+            # list_of_words.append(token)
 
-    doc["cosDist"] = cos_dist
+        number_of_documents += 1
 
-    pre = ""
-    suf = ""
+        with open(PROJECT_DIR + "data/vector_words_tags.json", "a") as example_docs_tags:
+            example_docs_tags.writelines(pre + json.dumps(doc).encode('utf-8') + suf + "\n")
 
-    if number_of_documents == 0:
-      pre = "["
-      suf = ","
-    elif number_of_documents == max_documents - 1:
-      suf = "]"
-    else:
-      suf = ","
-      #list_of_words.append(token)
+        if number_of_documents == max_documents:
+            break
 
-    number_of_documents += 1
+    ###########################################################################################
+    # sim = 0
+    # word = ""
+    #
+    # data = []
+    #
+    # tab_array = []
+    # row_array = []
+    #
+    # counter = 0
+    # for w in list_of_words:
+    #   data.append([])
+    #   data[counter].append(w)
+    #
+    #   # appending Word
+    #   row_array.append(w.encode("utf-8"))
+    #   # appending POS-Tag
+    #   row_array.append(w.encode("utf-8"))
+    #
+    #   try:
+    #     # appending top three words
+    #     top_three = model.most_similar(w, topn=3)
+    #     data[counter].append(top_three)
+    #
+    #     row_array.append(str(top_three[0][0]))
+    #     row_array.append(round(top_three[0][1], 4))
+    #     row_array.append(str(top_three[1][0]))
+    #     row_array.append(round(top_three[1][1], 4))
+    #     row_array.append(str(top_three[2][0]))
+    #     row_array.append(round(top_three[2][1], 4))
+    #
+    #   except:
+    #     try:
+    #       len(data[counter][1])
+    #     except:
+    #       data[counter].append([("-----", "-----"), ("-----", "-----"), ("-----", "-----")])
+    #
+    #     for x in range(0, 8 - len(row_array)):
+    #       # print (w + " is not in vocabulary!")
+    #       row_array.append("-----")
+    #
+    #   for f in fashion_words:
+    #     try:
+    #       # cos = model.similarity("/en/" + w[0], "/en/" + f[0])
+    #       cos = model.similarity(w.lower(), f[0].lower())
+    #       if cos > sim:
+    #         sim = cos
+    #         word = f[0]
+    #     except:
+    #       pass
+    #
+    #   # appending JOIN-Partner
+    #   if sim == 0:
+    #     data[counter].append("NONE")
+    #     row_array.append("NONE")
+    #   else:
+    #     data[counter].append(str(word) + "\n" + str(sim))
+    #     row_array.append(str(word) + "\n" + str(sim))
+    #
+    #     sim = 0
+    #
+    #
+    #
+    #   # add row to tab_array, reset row
+    #   tab_array.append(row_array)
+    #
+    #   counter += 1
+    #   row_array = []
 
-    with open(PROJECT_DIR + "data/vector_words_tags.json", "a") as example_docs_tags:
-      example_docs_tags.writelines(pre + json.dumps(doc).encode('utf-8') + suf + "\n")
+    print
+    print "###########################################################################################################################"
+    print "#                                            Textmining with Word2Vec and NLTK                                            #"
+    print "###########################################################################################################################"
 
-    if number_of_documents == max_documents:
-      break
+    # tab_array = collect_table_data(data)
+    # draw_table(tab_array)
 
-  #print(list_of_words)
+    print "Returning auto tagged json..."
+    with open(PROJECT_DIR + "data/vector_words_tags.json", "r") as text_file:
+        result = json.load(text_file)
+    return result
 
-  # load model for word2vec
-  #model = load_model(1)
-
-  ###########################################################################################
-  # sim = 0
-  # word = ""
-  #
-  # data = []
-  #
-  # tab_array = []
-  # row_array = []
-  #
-  # counter = 0
-  # for w in list_of_words:
-  #   data.append([])
-  #   data[counter].append(w)
-  #
-  #   # appending Word
-  #   row_array.append(w.encode("utf-8"))
-  #   # appending POS-Tag
-  #   row_array.append(w.encode("utf-8"))
-  #
-  #   try:
-  #     # appending top three words
-  #     top_three = model.most_similar(w, topn=3)
-  #     data[counter].append(top_three)
-  #
-  #     row_array.append(str(top_three[0][0]))
-  #     row_array.append(round(top_three[0][1], 4))
-  #     row_array.append(str(top_three[1][0]))
-  #     row_array.append(round(top_three[1][1], 4))
-  #     row_array.append(str(top_three[2][0]))
-  #     row_array.append(round(top_three[2][1], 4))
-  #
-  #   except:
-  #     try:
-  #       len(data[counter][1])
-  #     except:
-  #       data[counter].append([("-----", "-----"), ("-----", "-----"), ("-----", "-----")])
-  #
-  #     for x in range(0, 8 - len(row_array)):
-  #       # print (w + " is not in vocabulary!")
-  #       row_array.append("-----")
-  #
-  #   for f in fashion_words:
-  #     try:
-  #       # cos = model.similarity("/en/" + w[0], "/en/" + f[0])
-  #       cos = model.similarity(w.lower(), f[0].lower())
-  #       if cos > sim:
-  #         sim = cos
-  #         word = f[0]
-  #     except:
-  #       pass
-  #
-  #   # appending JOIN-Partner
-  #   if sim == 0:
-  #     data[counter].append("NONE")
-  #     row_array.append("NONE")
-  #   else:
-  #     data[counter].append(str(word) + "\n" + str(sim))
-  #     row_array.append(str(word) + "\n" + str(sim))
-  #
-  #     sim = 0
-  #
-  #
-  #
-  #   # add row to tab_array, reset row
-  #   tab_array.append(row_array)
-  #
-  #   counter += 1
-  #   row_array = []
-
-  print
-  print "###########################################################################################################################"
-  print "#                                            Textmining with Word2Vec and NLTK                                            #"
-  print "###########################################################################################################################"
-
-  # tab_array = collect_table_data(data)
-  #draw_table(tab_array)
-
-  return " "
-
-
-# returns an list of words from a given array of sentences
 def nltk_tokenizing(document):
-  #
-  # NLTK Tokenizing
-  #
-  list_of_words = []
+    """
+    Takes a JSON-Document, which contains plaintext at key "extracted_text" and returns a list of single words
+    after tokenizing with NLTK
+    :param document:
+    :return: list of words
+    """
+    #
+    # NLTK Tokenizing
+    #
+    list_of_words = []
 
-  # for x in range(0, 1):
-  # reads first json
-  #json_data = json.loads(document)
+    for data in document:
+        print(data)
+        extracted_text = data["extracted_text"]
+        sentences = nltk.sent_tokenize(extracted_text)
 
-  for data in document:
-    print(data)
-    extracted_text = data["extracted_text"]
-    sentences = nltk.sent_tokenize(extracted_text)
-
-    for s in sentences:
-      for word in (nltk.tag.pos_tag(nltk.word_tokenize(s))):
-        if word[1] == "NN" or word[1] == "NNP" or word[1] == "NNPS" or word[1] == "NNS":
-          list_of_words.append(word)
-  return list_of_words
-
-
-  # for x in range(0, 2):
-  #   # reads first json
-  #   json_data = json.loads(text[x])
-  #
-  #   # print(json_data)
-  #   extracted_text = json_data["extracted_text"]
-  #
-  #   # Sentence Tokenizing
-  #   sentences = nltk.sent_tokenize(extracted_text)
-  #   # print(sentences)
-  #   for s in sentences:
-  #     # Word tokenizing and tagging
-  #     for word in (nltk.tag.pos_tag(nltk.word_tokenize(s))):
-  #         list_of_words.append(word)
-  #     #for word in tok_words:
-  #     #  list_of_words.append(word)
+        for s in sentences:
+            for word in (nltk.tag.pos_tag(nltk.word_tokenize(s))):
+                if word[1] == "NN" or word[1] == "NNP" or word[1] == "NNPS" or word[1] == "NNS":
+                    list_of_words.append(word)
+    return list_of_words
 
 
-# loads a word2vec model depending on x:
-# x = 0: Glove model
-# x = 1: Own Fashion Model
-# x = 2: Freebase TODO: Path
 def load_model(x):
-  if x == 0:
-    '''
-    Convert Glove Model to Gensim Word2Vec
-    GloVe is another algorithm that creates vector representations of words similar to word2vec.
-    GloVe transforms the neutral network problem into a word co-occurrence matrix so it should
-    be faster to train but uses more memory.
-    '''
+    """
+    loads a model which depends on the given parameter:
+    0: Glove Model
+    1: Own Fashion Model created from Zalando Documents
+    :param x: integer
+    :return: path of model
+    """
+    if x == 0:
+        '''
+        Convert Glove Model to Gensim Word2Vec
+        GloVe is another algorithm that creates vector representations of words similar to word2vec.
+        GloVe transforms the neutral network problem into a word co-occurrence matrix so it should
+        be faster to train but uses more memory.
+        '''
 
-    def any2unicode(text, encoding='utf8', errors='strict'):
-      if isinstance(text, unicode):
-        return text
-      return unicode(text.replace('\xc2\x85', '<newline>'), encoding, errors=errors)
+        def any2unicode(text, encoding='utf8', errors='strict'):
+            if isinstance(text, unicode):
+                return text
+            return unicode(text.replace('\xc2\x85', '<newline>'), encoding, errors=errors)
 
-    gensim.models.utils.to_unicode = any2unicode
+        gensim.models.utils.to_unicode = any2unicode
 
-    return gensim.models.Word2Vec.load_word2vec_format(join(GLOVE_DIR, 'common.840B.300d.txt'), binary=False)
+        return gensim.models.Word2Vec.load_word2vec_format(join(GLOVE_DIR, 'common.840B.300d.txt'), binary=False)
 
-  if x == 1:
-    return gensim.models.Word2Vec.load(PROJECT_DIR + 'data/models/fashion_model')
+    if x == 1:
+        return gensim.models.Word2Vec.load(PROJECT_DIR + 'data/models/fashion_model')
 
-  if x == 2:
-    return gensim.models.Word2Vec.load_word2vec_format('/opt/word2vec/freebase_model_en.bin.gz', binary=True)
+    #if x == 2:
+    #    return gensim.models.Word2Vec.load_word2vec_format('/opt/word2vec/freebase_model_en.bin.gz', binary=True)
 
-
+# TODO collecting table data
 def collect_table_data(data):
-  result = []
-  row_array = []
-
-  for line in data:
-    # row_array.append(line[0][0])
-    # row_array.append(line[0][1])
-    # row_array.append(str(line[1][0][0]).encode('utf8'))
-    # row_array.append(str(line[1][0][1]))
-    # row_array.append(str(line[1][1][0]).encode('utf8'))
-    # row_array.append(str(line[1][1][1]))
-    # row_array.append(str(line[1][2][0]).encode('utf8'))
-    # row_array.append(str(line[1][2][1]))
-    # row_array.append(line[2])
-    # result.append(row_array)
+    result = []
     row_array = []
 
-  return result
+    for line in data:
+        # row_array.append(line[0][0])
+        # row_array.append(line[0][1])
+        # row_array.append(str(line[1][0][0]).encode('utf8'))
+        # row_array.append(str(line[1][0][1]))
+        # row_array.append(str(line[1][1][0]).encode('utf8'))
+        # row_array.append(str(line[1][1][1]))
+        # row_array.append(str(line[1][2][0]).encode('utf8'))
+        # row_array.append(str(line[1][2][1]))
+        # row_array.append(line[2])
+        # result.append(row_array)
+        row_array = []
+
+    return result
 
 
 # draws a table
 def draw_table(tab_array):
-  tab = tt.Texttable()
-  tab.header(['Words', 'POS-Tag', 'Word1', 'Cos-Dist', 'Word2', 'Cos-Dist', 'Word3', 'Cos-Dist', 'JOIN-Partner'])
+    tab = tt.Texttable()
+    tab.header(['Words', 'POS-Tag', 'Word1', 'Cos-Dist', 'Word2', 'Cos-Dist', 'Word3', 'Cos-Dist', 'JOIN-Partner'])
 
-  for row in tab_array:
-    tab.add_row(row)
+    for row in tab_array:
+        tab.add_row(row)
 
-  # tab.add_row(['Zalando', 'NN', 'H&M',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'shoe'])
-  # tab.add_row(['is', 'VB', 'word2',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'blub'])
-  # tab.add_row(['big', 'AD', 'word2',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'fubar'])
+    # tab.add_row(['Zalando', 'NN', 'H&M',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'shoe'])
+    # tab.add_row(['is', 'VB', 'word2',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'blub'])
+    # tab.add_row(['big', 'AD', 'word2',	'0,6434', 'word2',	'0,6234', 'word3', '0,5324', 'fubar'])
 
-  tab.set_cols_width([15, 5, 15, 5, 15, 5, 15, 5, 15])
-  tab.set_cols_align(['l', 'l', 'l', 'l', 'l', 'l', 'l', 'l', 'l'])
-  tab.set_chars(['-', '|', '+', '='])
+    tab.set_cols_width([15, 5, 15, 5, 15, 5, 15, 5, 15])
+    tab.set_cols_align(['l', 'l', 'l', 'l', 'l', 'l', 'l', 'l', 'l'])
+    tab.set_chars(['-', '|', '+', '='])
 
-  print tab.draw()
+    print tab.draw()
 
 
 def custom_public_function_reachable_from_outside():
-  """define functions that can be accessed from main.py and other modules"""
+    """define functions that can be accessed from main.py and other modules"""
 
 
 if __name__ == "__main__":
-  # Execute the main function if this file was executed from the terminal
-  word2vec()
-  # gloveToVec()
+    # Execute the main function if this file was executed from the terminal
+    word2vec()
